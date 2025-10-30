1f95dd39e441df7db9db90d182e5dc5ec5ed61cc
MatheusVianaHenriquesCotemig <22400265@aluno.cotemig.com.br>
2025-10-30 16:19:29 -0300

    Add files via upload

---
+++ javaScript/botoes.js
@@ -0,0 +1,12 @@
+function sobreBtn(){
+    window.location.href = "sobre.html"
+}
+function habilidadesBtn(){
+    window.location.href = "habilidades.html"
+}
+function projetosBtn(){
+    window.location.href = "https://github.com/MatheusVianaHenriquesCotemig"
+}
+function contatosBtn(){
+    window.location.href = "contatos.html"
+}
